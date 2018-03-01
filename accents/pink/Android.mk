41fb4be7950fb11727c8fc387744637ea49d9db5
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-03-01 15:13:45 +0200

    Rename overlays

--- accents/pink/Android.mk
+++ accents/pink/Android.mk
@@ -1,9 +1,10 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := ElixirPinkAccent
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := LineagePinkAccent
+LOCAL_PRIVILEGED_MODULE := false
 
 include $(BUILD_PACKAGE)
