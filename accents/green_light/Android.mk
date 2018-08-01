1221a16511c3cf1d52cce0c4c5b5bb18c52de161
Razziell <razziell@ya.ru>
2018-08-01 21:34:29 +0300

    Rework accents [3/4]

--- accents/green_light/Android.mk
+++ accents/green_light/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := MDroidVioletAccent
+LOCAL_PACKAGE_NAME := MDroidGreenLightAccent
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
