50028ff08f3954f623d6679e7095a5858704f0ac
Hikari no Tenshi <kyryljan.serhij@gmail.com>
2018-12-25 17:25:02 +0200

    Update Black and Dark overlays

--- ContactsDarkTheme/Android.mk
+++ ContactsDarkTheme/Android.mk
@@ -4,5 +4,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := MDroidContactsDarkTheme
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/Contacts_intermediates/package.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/Contacts_intermediates/package.apk
 LOCAL_PRIVILEGED_MODULE := false
 include $(BUILD_PACKAGE)
