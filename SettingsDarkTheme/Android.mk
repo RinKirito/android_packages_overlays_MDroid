03546ce9f30f85e5197cbda063e6b072f02ed41a
Hikari no Tenshi <kyryljan.serhij@gmail.com>
2019-01-11 12:24:01 +0200

    Update Settings Black & Dark themes
    Fixes for fingerprint settings styles.

--- SettingsDarkTheme/Android.mk
+++ SettingsDarkTheme/Android.mk
@@ -5,6 +5,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := MDroidSettingsDarkTheme
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/Settings_intermediates/package.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/Settings_intermediates/package.apk
 LOCAL_PRIVILEGED_MODULE := false
 
 include $(BUILD_PACKAGE)
