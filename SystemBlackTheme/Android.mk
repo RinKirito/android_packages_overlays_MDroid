5fc1ded7f26fc673a2be86682e69758f6489c142
Hikari no Tenshi <kyryljan.serhij@gmail.com>
2019-01-18 14:47:43 +0200

    Customize date and time pickers for Black theme

--- SystemBlackTheme/Android.mk
+++ SystemBlackTheme/Android.mk
@@ -5,6 +5,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := MDroidSystemBlackTheme
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/framework-res_intermediates/package.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/framework-res_intermediates/package.apk
 LOCAL_PRIVILEGED_MODULE := false
 
 include $(BUILD_PACKAGE)
